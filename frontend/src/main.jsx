30080294170b068df3175194ccaa54976922171d
chirayu <chirayujain362005@gmail.com>
2026-05-08 00:19:51 +0530

    complete deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import '@solana/wallet-adapter-react-ui/styles.css'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+import { RPC_ENDPOINT } from './constants/config'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const wallets = [new PhantomWalletAdapter()]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConnectionProvider endpoint={RPC_ENDPOINT}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <App />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  </React.StrictMode>
 )
